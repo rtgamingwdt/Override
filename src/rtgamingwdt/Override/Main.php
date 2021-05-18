b04cd9465a12bad305e6fb7d554809106f7517cf
RT <46906497+rtgamingwdt@users.noreply.github.com>
2021-05-18 02:15:33 -0400

    Update Main.php

--- src/rtgamingwdt/Override/Main.php
+++ src/rtgamingwdt/Override/Main.php
@@ -10,9 +10,8 @@ class Main extends PluginBase {
     
   public function onEnable() {    
     self::$main = $this;
-    $this->unregister("ban", "ban-ip");
+    $this->unregister("ban");
     $this->getServer()->getCommandMap()->register("ban", new commands\BanCommand($this));
-    $this->getServer()->getCommandMap()->register("ban-ip", new commands\BanIpCommand($this));
     $this->getLogger()->info("§eOverride by RT has been §aenabled!");
   }
 
