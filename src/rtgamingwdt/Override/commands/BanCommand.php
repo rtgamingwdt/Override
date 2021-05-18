6bb573343d96ddea0771dcabf938f42122716ca2
RT <46906497+rtgamingwdt@users.noreply.github.com>
2021-05-18 01:30:30 -0400

    Update BanCommand.php

--- src/rtgamingwdt/Override/commands/BanCommand.php
+++ src/rtgamingwdt/Override/commands/BanCommand.php
@@ -56,17 +56,15 @@ class BanCommand extends PluginCommand {
             
             $reason = implode(" ", $args);
             
-            if(count($args) !== 0) {
+            if(count($args) < 0) {
                 $sender->sendMessage("Please specify who you want to ban.");
-                return true;
-            }
+            } else {            
+                $sender->getServer()->getNameBans()->addBan($name, $reason, null, $sender->getName());
+                $sender->sendMessage(TextFormat::GREEN . $name . " has been banned for " . $reason);
             
-            $sender->getServer()->getNameBans()->addBan($name, $reason, null, $sender->getName());
-            
-            $sender->sendMessage(TextFormat::GREEN . $name . " has been banned for " . $reason);
-            
-            if(($player = $sender->getServer()->getPlayerExact($name)) instanceof Player) {
-                $player->kick($reason !== "" ? "You have been banned for " . $reason : "No reason specified.");
+                if(($player = $sender->getServer()->getPlayerExact($name)) instanceof Player) {
+                    $player->kick($reason !== "" ? "You have been banned for " . $reason : "No reason specified.");
+                }
             }
         }
     }
