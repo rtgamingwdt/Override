16e3fcee4f98a0af736d47da7b8f083f0f79045c
RT <46906497+rtgamingwdt@users.noreply.github.com>
2021-05-19 15:21:58 -0400

    Create PlayerLoginEvent.php

---
+++ src/rtgamingwdt/Override/events/PlayerLoginEvent.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace rtgamingwdt\Override\events;
+
+use pocketmine\event\player\PlayerPreLoginEvent;
+use pocketmine\permission\BanList;
+use pocketmine\permission\BanEntry;
+
+class PlayerLoginEvent implements Listener {
+  
+  public function onJoin(PlayerPreLoginEvent $event) {    
+    // Hope this works. If there are bugs with your server that include banning. Remove the plugin from your server. I will try to get it working eventually if I see any bugs.
+    $player = $event->getPlayer();
+    $name = $player->getName();
+    $entry = new BanEntry($name);
+    
+    if($player->isBanned()) {
+      $player->close("", "§8Failed to connect to the server \n§cYou are banned from this server! \n§8Reason: §f" . $entry->getReason() . "§8If you have any questions. Please visit our Discord Server at https://discord.gg/A624QQjgJA
+ \n §8Ban ID: §f" . $player->getUniqueId() . "\n §8Make sure not to share your ban ID with anyone other than a offical FortressFusion staff member. Doing so may slow down the process of you getting unbanned.");
+    }
+  }
+}
