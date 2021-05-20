f4d0d65082715d949d2f25b36f3a0f01785f9602
RT <46906497+rtgamingwdt@users.noreply.github.com>
2021-05-20 10:14:51 -0400

    Update DefaultGamemodeCommand.php

--- src/rtgamingwdt/Override/commands/DefaultGamemodeCommand.php
+++ src/rtgamingwdt/Override/commands/DefaultGamemodeCommand.php
@@ -19,25 +19,27 @@ use function implode;
 
 class DefaultGamemodeCommand extends PluginCommand {
   // Might update this command in the future.
-  public function __construct() {
-    parent::__construct("defaultgamemode", Main::getMain());  
-    $this->setDescription("Set the default gamemode");
-    $this->setPermission("cmd.defaultgamemode");
-    }
+	public function __construct() {
+		parent::__construct("defaultgamemode", Main::getMain());  
+		$this->setDescription("Set the default gamemode");
+		$this->setPermission("cmd.defaultgamemode");
+	}
     
-    function execute(CommandSender $sender, string $commandLabel, array $args) {
-      if(count($args) === 0){
-        $sender->sendMessage(TextFormat::RED . "Are you serious? You cannot set a empty gamemode as the default gamemode");
-        return true;
-      }
+	function execute(CommandSender $sender, string $commandLabel, array $args) {
+		if($sender->isOp()) {
+			if(count($args) === 0){
+				$sender->sendMessage(TextFormat::RED . "Are you serious? You cannot set a empty gamemode as the default gamemode");
+				return true;
+			}
       
-      $gameMode = $sender->getServer()->getGamemodeFromString($args[0]);
-      
-      if($gameMode !== -1){
-        $sender->getServer()->setConfigInt("gamemode", $gameMode);
-        $sender->sendMessage(TextFormat::GREEN . "The default gamemode has been set to " . $sender->getServer()->getGamemodeString($gameMode));
-		}else{
-        $sender->sendMessage(TextFormat::RED . "Could not find that gamemode. Are you sure it exists?");
-      }
-    }
+			$gameMode = $sender->getServer()->getGamemodeFromString($args[0]);
+		
+			if($gameMode !== -1){
+				$sender->getServer()->setConfigInt("gamemode", $gameMode);
+				$sender->sendMessage(TextFormat::GREEN . "The default gamemode has been set to " . $sender->getServer()->getGamemodeString($gameMode));
+			}else{
+				$sender->sendMessage(TextFormat::RED . "Could not find that gamemode. Are you sure it exists?");
+			}
+		}
+	}
 }
